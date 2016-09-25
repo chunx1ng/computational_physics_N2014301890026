d7ba17736f2caa0d8138bc1beade07bc0a34fd5f
chunx1ng <609289948@qq.com>
2016-09-25 20:57:56 +0800

    Update ex03_2.py

--- Exercise_03-Let-your-name-move-Let-an-object-spin/ex03_2.py
+++ Exercise_03-Let-your-name-move-Let-an-object-spin/ex03_2.py
@@ -1,1 +1,31 @@
+import os
+import time
+i=1
+os.system('cls')
+while i<=4:
+    print('                                                                ')
+    print('                                                                ')
+    print('                             #                                  ')
+    print('                      ##//       \\##                           ')
+    print('                  ##/                 \##                       ')
+    print('                #            .            #                     ')
+    print('                  ##\                 /##                       ')
+    print('                      ##\\        //##                          ')
+    print('                             #                                  ')
+    time.sleep(0.5)
+    os.system('cls')
+    print('                             #                                  ')              
+    print('                           #   #                                ')
+    print('                          #     #                               ')
+    print('                         #       #                              ')
+    print('                        #         #                             ')
+    print('                       #     .     #                            ')
+    print('                        #         #                             ')
+    print('                         #       #                              ')
+    print('                          #     #                               ')
+    print('                           #   #                                ')
+    print('                             #                                  ')
+    time.sleep(0.5)
+    os.system('cls')
+    i=i+1
 
